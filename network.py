07425872829a0cda9cdcfb6f304517e19f2ffff6
Dominik Kallusky <dominik.kallusky@gmail.com>
2018-09-22 23:51:21 -0700

    gpu now

--- network.py
+++ network.py
@@ -295,10 +295,10 @@ def classify_next_item(game_config, network_config):
     # net = dropout(net, 0.7)
 
     net = relu(
-        batch_normalization(fully_connected(final_input_layer, 1024, bias=False, activation=None, regularizer="L2")))
+        batch_normalization(fully_connected(final_input_layer, 256, bias=False, activation=None, regularizer="L2")))
     for i in range(5):
-        net = highway(net, 1024, activation='elu', regularizer="L2", transform_dropout=0.8)
-        net = dropout(net, 0.8)
+        net = highway(net, 256, activation='elu', regularizer="L2", transform_dropout=0.8)
+        net = dropout(net, 0.6)
 
     # net = fully_connected(final_input_layer, 256, activation='relu', regularizer='L2')
     net = fully_connected(net, total_num_items, activation='softmax')
