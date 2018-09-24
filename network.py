9d0e1d2af5e657f0dbb5e34c72282e6d76fa4de1
Dominik Kallusky <dominik.kallusky@gmail.com>
2018-09-23 22:48:23 -0700

    gpu now

--- network.py
+++ network.py
@@ -286,7 +286,7 @@ def classify_next_item(game_config, network_config):
                                   scope="team_sum_scope")
 
     final_input_layer = merge([items_by_champ_k_hot, summed_items_by_champ, champs, team1_score, team2_score], mode='concat', axis=1)
-    net = dropout(final_input_layer, 0.8)
+    net = dropout(final_input_layer, 0.9)
     # net = relu(
     #     batch_normalization(fully_connected(final_input_layer, 256, bias=False, activation=None, regularizer="L2")))
     # net = dropout(net, 0.7)
@@ -296,10 +296,10 @@ def classify_next_item(game_config, network_config):
 
     net = relu(
         batch_normalization(fully_connected(net, 512, bias=False, activation=None, regularizer="L2")))
-    net = dropout(net, 0.7)
+    net = dropout(net, 0.8)
     net = relu(
         batch_normalization(fully_connected(net, 256, bias=False, activation=None, regularizer="L2")))
-    net = dropout(net, 0.6)
+    net = dropout(net, 0.7)
 
 
     # for i in range(5):
