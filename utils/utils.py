26bf7c8a9f00ed699bc4bdbedac95ef55319c3b9
Dominik Kallusky <dominik.kallusky@gmail.com>
2019-07-28 04:38:44 -0700

    RADS folder is not in every league installation

--- utils/utils.py
+++ utils/utils.py
@@ -57,7 +57,7 @@ def get_lol_dir():
                 loldir = f.read()
         else:
             loldir = "C:/Riot Games/League of Legends"
-        if not os.path.isdir(loldir + "/RADS"):
+        if not os.path.isdir(loldir + "/Game"):
             query_lol_dir()
         else:
             return loldir
@@ -69,8 +69,8 @@ def query_lol_dir():
                         "We were unable to locate your League of Legends installation. Please select your main League of Legends folder.")
     loldir = askdirectory(initialdir="C:", title="Please select your main League of Legends folder")
     
-    while not os.path.isdir(loldir + "/RADS"):
-        messagebox.showinfo("Information", "That wasn't it. Select the folder that has the RADS folder in it.")
+    while not os.path.isdir(loldir + "/Game"):
+        messagebox.showinfo("Information", "That wasn't it. Select the folder that has the Game folder in it.")
         loldir = askdirectory(initialdir="C:", title="Please select your main League of Legends folder")
         if loldir == "":
             sys.exit()
