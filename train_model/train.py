a28d92050e2a7d3ebc38a792be31a2a8710d59f4
Dominik Kallusky <dominik.kallusky@gmail.com>
2020-02-08 22:26:00 -0800

    f

--- train_model/train.py
+++ train_model/train.py
@@ -711,13 +711,13 @@ class NextItemsTrainer(Trainer):
                                                                "next_items_processed_elite_sorted_complete"])
         dataloader_lower = data_loader.SortedNextItemsDataLoader(app_constants.train_paths[
                                                                      "next_items_processed_lower_sorted_complete"])
-        X_elite, Y_elite = dataloader_elite.get_train_data(self.only_full_items_completed)
+        X_elite, Y_elite = dataloader_elite.get_train_data(NextItemsTrainer.only_full_items_completed)
         print("Loading test data")
-        X_test_elite, Y_test_elite = dataloader_elite.get_test_data(self.only_full_items_completed)
+        X_test_elite, Y_test_elite = dataloader_elite.get_test_data(NextItemsTrainer.only_full_items_completed)
 
-        X_lower, Y_lower = dataloader_lower.get_train_data(self.only_full_items_completed)
+        X_lower, Y_lower = dataloader_lower.get_train_data(NextItemsTrainer.only_full_items_completed)
         print("Loading test data")
-        X_test_lower, Y_test_lower = dataloader_lower.get_test_data(self.only_full_items_completed)
+        X_test_lower, Y_test_lower = dataloader_lower.get_test_data(NextItemsTrainer.only_full_items_completed)
 
         self.X = np.concatenate([X_elite, X_lower], axis=0)
         self.Y = np.concatenate([Y_elite, Y_lower], axis=0)
