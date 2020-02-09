3cc3f41ce23b6adf0a96a321f7ddc8e0198894da
Dominik Kallusky <dominik.kallusky@gmail.com>
2020-02-08 21:15:53 -0800

    f

--- train_model/train.py
+++ train_model/train.py
@@ -676,6 +676,7 @@ class NextItemsTrainer(Trainer):
         # dump(min_max_scaler, self.best_path + scaler_name + "_scaler", compress=True)
         return min_max_scaler
 
+
     @staticmethod
     def no_full_items_completed(data):
         pos = data[:, 1]
@@ -685,6 +686,17 @@ class NextItemsTrainer(Trainer):
         target_summ_items = data_items[range(len(pos)), pos, ::2]
         target_summs_full_items_boolean = np.isin(target_summ_items, list(full_item_ints))
         return np.logical_not(np.any(target_summs_full_items_boolean, axis=1))
+
+
+    @staticmethod
+    def only_full_items_completed(data):
+        pos = data[:, 1]
+        full_item_ints = ItemManager().get_full_item_ints()
+        data_items = data[:, 12:6 * 12]
+        data_items = np.reshape(data_items, (-1, 5, 12))
+        target_summ_items = data_items[range(len(pos)), pos, ::2]
+        target_summs_full_items_boolean = np.isin(target_summ_items, list(full_item_ints))
+        return np.any(target_summs_full_items_boolean, axis=1)
 
 
     def build_next_items_late_game_model(self):
@@ -699,13 +711,13 @@ class NextItemsTrainer(Trainer):
                                                                "next_items_processed_elite_sorted_complete"])
         dataloader_lower = data_loader.SortedNextItemsDataLoader(app_constants.train_paths[
                                                                      "next_items_processed_lower_sorted_complete"])
-        X_elite, Y_elite = dataloader_elite.get_train_data(lambda _: np.logical_not(self.no_full_items_completed))
+        X_elite, Y_elite = dataloader_elite.get_train_data(self.only_full_items_completed)
         print("Loading test data")
-        X_test_elite, Y_test_elite = dataloader_elite.get_test_data(lambda _: np.logical_not(self.no_full_items_completed))
+        X_test_elite, Y_test_elite = dataloader_elite.get_test_data(self.only_full_items_completed)
 
-        X_lower, Y_lower = dataloader_lower.get_train_data(lambda _: np.logical_not(self.no_full_items_completed))
+        X_lower, Y_lower = dataloader_lower.get_train_data(self.only_full_items_completed)
         print("Loading test data")
-        X_test_lower, Y_test_lower = dataloader_lower.get_test_data(lambda _: np.logical_not(self.no_full_items_completed))
+        X_test_lower, Y_test_lower = dataloader_lower.get_test_data(self.only_full_items_completed)
 
         self.X = np.concatenate([X_elite, X_lower], axis=0)
         self.Y = np.concatenate([Y_elite, Y_lower], axis=0)
