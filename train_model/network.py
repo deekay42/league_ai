dcf7f252db53338aa95d1384039f53af9013811f
Dominik Kallusky <dominik.kallusky@gmail.com>
2020-02-21 18:48:00 -0800

    f

--- train_model/network.py
+++ train_model/network.py
@@ -991,7 +991,11 @@ class NextItemLateGameNetwork(NextItemNetwork):
                 target_summ_current_gold
             ], mode='concat', axis=1)
 
-        net = batch_normalization(fully_connected(nonstarter_input_layer, 128, bias=False,
+        net = batch_normalization(fully_connected(nonstarter_input_layer, 256, bias=False,
+                                                  activation='relu',
+                                                  regularizer="L2"))
+        net = dropout(net, 0.8)
+        net = batch_normalization(fully_connected(net, 128, bias=False,
                                                   activation='relu',
                                                   regularizer="L2"))
         net = dropout(net, 0.9)
