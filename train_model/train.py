ac0c8b1b868a9159e1e13f901581df6d6573588d
Dominik Kallusky <dominik.kallusky@gmail.com>
2020-03-03 10:44:28 -0800

    f

--- train_model/train.py
+++ train_model/train.py
@@ -646,9 +646,9 @@ class NextItemsTrainer(Trainer):
 
         print("Loading training data")
         dataloader = data_loader.SortedNextItemsDataLoader(app_constants.train_paths[
-                                                               "next_items_processed_elite_sorted_uninf"])
+                                                               "next_items_processed_elite_sorted_inf"])
         dataloader_lower = data_loader.SortedNextItemsDataLoader(app_constants.train_paths[
-                                                               "next_items_processed_lower_sorted_uninf"])
+                                                               "next_items_processed_lower_sorted_inf"])
         self.X, self.Y = dataloader.get_train_data()
         print("Loading test data")
         self.X_test, self.Y_test = dataloader.get_test_data()
