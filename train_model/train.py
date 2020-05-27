79c157c2cb2538fefe7ccdba73450d9da0b3c8bc
Dominik Kallusky <dominik.kallusky@gmail.com>
2020-05-27 13:14:25 -0700

    f

--- train_model/train.py
+++ train_model/train.py
@@ -962,20 +962,20 @@ class NextItemsTrainer(Trainer):
         print("Loading training data")
         dataloader_elite = data_loader.SortedNextItemsDataLoader(app_constants.train_paths[
                                                                      "next_items_processed_elite_sorted_inf"])
-        dataloader_lower = data_loader.SortedNextItemsDataLoader(app_constants.train_paths[
-                                                                     "next_items_processed_lower_sorted_inf"])
+        # dataloader_lower = data_loader.SortedNextItemsDataLoader(app_constants.train_paths[
+        #                                                              "next_items_processed_lower_sorted_inf"])
         X_elite, Y_elite = dataloader_elite.get_train_data()
         print("Loading test data")
         X_test_elite, Y_test_elite = dataloader_elite.get_test_data()
 
-        X_lower, Y_lower = dataloader_lower.get_train_data()
-        print("Loading test data")
-        X_test_lower, Y_test_lower = dataloader_lower.get_test_data()
-
-        self.X = np.concatenate([X_elite, X_lower], axis=0)
-        self.Y = np.concatenate([Y_elite, Y_lower], axis=0)
-        self.X_test = np.concatenate([X_test_elite, X_test_lower], axis=0)
-        self.Y_test = np.concatenate([Y_test_elite, Y_test_lower], axis=0)
+        # X_lower, Y_lower = dataloader_lower.get_train_data()
+        # print("Loading test data")
+        # X_test_lower, Y_test_lower = dataloader_lower.get_test_data()
+        #
+        # self.X = np.concatenate([X_elite, X_lower], axis=0)
+        # self.Y = np.concatenate([Y_elite, Y_lower], axis=0)
+        # self.X_test = np.concatenate([X_test_elite, X_test_lower], axis=0)
+        # self.Y_test = np.concatenate([Y_test_elite, Y_test_lower], axis=0)
 
         self.train_y_distrib = Counter(self.Y)
         self.test_y_distrib = Counter(self.Y_test)
@@ -1711,14 +1711,14 @@ if __name__ == "__main__":
     #                           "opp_champ_embs_dst")
 
 
-    # t = NextItemsTrainer()
-    # t.build_next_items_standard_game_model()
+    t = NextItemsTrainer()
+    t.build_next_items_standard_game_model()
 
     # t = NextItemsTrainer()
     # t.build_next_items_late_game_model()
 
-    t = WinPredTrainer()
-    t.train()
+    # t = WinPredTrainer()
+    # t.train()
     # t = BootsTrainer()
     # t.train()
     # t = StarterItemsTrainer()
